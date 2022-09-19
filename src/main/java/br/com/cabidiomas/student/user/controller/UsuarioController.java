0846898ad63480037a878f3e530201175c0c77e7
alexs <alexstlopes@gmail.com>
2022-09-19 20:22:55 -0300

    Ajustado retorno da paginação

--- src/main/java/br/com/cabidiomas/student/user/controller/UsuarioController.java
+++ src/main/java/br/com/cabidiomas/student/user/controller/UsuarioController.java
@@ -55,7 +55,7 @@ public class UsuarioController {
 
         return new PageImpl<>(
                 usuarioDtos,
-                pageRequest, userPage.getSize());
+                pageRequest, userPage.getTotalElements());
     }
 
     @PutMapping("{id}")
